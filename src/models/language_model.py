7be612873f8bc198e3a0a21f02d8b6756670eae1
Rafaela Plinta <ra.plinta@gmail.com>
2023-12-11 09:25:54 -0300

    feat: Cria o método to_dict

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -7,3 +7,12 @@ class LanguageModel(AbstractModel):
 
     def __init__(self, data):
         super().__init__(data)
+
+    def to_dict(self):
+        # get é um método de dicts. Ele possibilita a obtenção de um valor
+        # associado a uma chave específíca.
+        # https://www.w3schools.com/python/ref_dictionary_get.asp
+        return {
+            "name": self.data.get("name"),
+            "acronym": self.data.get("acronym"),
+        }
