6e5bb171bac1819ac723153eff08be4d77a0abda
Rafaela Plinta <ra.plinta@gmail.com>
2023-12-12 09:29:43 -0300

    feat: cria testes para history_model

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,23 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
-# Req. 7
-def test_request_history():
-    raise NotImplementedError
+# recebe prepare_base como argumento para os testes
+def test_request_history(prepare_base):
+    history_model = HistoryModel.list_as_json()
+    # converte o JSON retornado para um objeto Python usando json.loads
+    json_data = json.loads(history_model)
+
+    assert len(json_data) == 2
+
+    first_item = json_data[0]
+
+    # verifica se as chaves existem
+    assert "text_to_translate" in first_item
+    assert "translate_from" in first_item
+    assert "translate_to" in first_item
+
+    # verifica se os valores correspondem ao esperado
+    assert first_item["text_to_translate"] == "Hello, I like videogame"
+    assert first_item["translate_from"] == "en"
+    assert first_item["translate_to"] == "pt"
