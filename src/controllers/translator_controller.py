ce4af0316683d35e00175664badf91f67ee8c415
Rafaela Plinta <ra.plinta@gmail.com>
2023-12-12 08:23:15 -0300

    feat: Adiciona a rota post /reverse

--- src/controllers/translator_controller.py
+++ src/controllers/translator_controller.py
@@ -39,8 +39,7 @@ def translate_text():
     # a biblioteca GoogleTranslator traduz o texto usando os idiomas de origem
     # e destino
     translated = GoogleTranslator(
-        source=translate_from,
-        target=translate_to
+        source=translate_from, target=translate_to
     ).translate(text_to_translate)
 
     return render_template(
@@ -50,4 +49,26 @@ def translate_text():
         translate_from=translate_from,
         translate_to=translate_to,
         translated=translated,
+    )
+
+
+@translator_controller.route("/reverse", methods=["POST"])
+def reverse_translation():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    reversed_translated = GoogleTranslator(
+        source=translate_from,
+        target=translate_to,
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=reversed_translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
     )
